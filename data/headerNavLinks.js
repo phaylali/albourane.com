79ccabaceae079d20c6f0069c18acc37d014ddf5
Phaylali <admin@omniversify.com>
2026-02-12 02:53:48 +0100

    feat: Remove various content, data, and development configuration files.

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'مدونة ' },
-  { href: '/tags', title: 'مواضيع ' },
-  { href: '/projects', title: 'مشاريع ' },
-  { href: '/about', title: 'من نحن ' },
-]
-
-export default headerNavLinks
